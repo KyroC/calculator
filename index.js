5ae257df820505da55dcc8aab4fc5f6f0ebc7a04
KyroC <kyroeth@gmail.com>
2022-06-24 07:05:29 -0700

    Added all working functions

--- index.js
+++ index.js
@@ -15,24 +15,75 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b
 }
-//Operate, (a, b, operator)
-function operate(a,b,operator){
-    return operator(parseInt(a),parseInt(b))
-}
 
-//get user input
-var userInput = ""
+//Calculator Display
+const display = document.getElementById("display")
+
+//get user input 1
+let userInput = ""
+let currentNum = ""
+let symbol = ""
 
 function input(a) {
     userInput = userInput + a
     display.innerHTML = userInput
 }
+//Operate, (a, b, operator)
+function operate(a,b,operator){
+    a = currentNum
+    b = userInput
 
+    if (userInput == ""){
+        userInput = userInput
+        symbol = operator;
+    }else if (currentNum != "" && symbol == ""){
+        symbol = operator;
+        currentNum = userInput
+        userInput = ""
+    }
+    else if(currentNum == ""){
+        currentNum = userInput;
+        //console.log("user input is " + userInput)
+        userInput = ""
+        //console.log("current value is " + currentNum)
+        display.innerHTML = parseFloat(currentNum).toFixed(2);
+        symbol = operator
+    }
+    else {
+        currentNum = window[symbol](parseFloat(a), parseFloat(b))
+        //console.log("user input is " + userInput)
+        userInput=""
+        //console.log("current value is " + currentNum)
+        display.innerHTML = parseFloat(currentNum).toFixed(2);
+        symbol = operator
+    }
+}
+function equate(a,b){
+    if (userInput == ""){
+        userInput = userInput
+    }
+    else if (currentNum == "" ) {
+        currentNum = ""
+    }
+    else {
+        //console.log(symbol)
+        a = currentNum
+        b = userInput
+        currentNum = window[symbol](parseFloat(a),parseFloat(b))
+        display.innerHTML = parseFloat(currentNum).toFixed(2)
+        userInput = ""
+        symbol = ""
+        //console.log(currentNum)
+    }
+}
+function clearCalc() {
+    currentNum = ""
+    userInput = ""
+    symbol = ""
+    //console.log("current value is " + currentNum)
+    //console.log("user input is " + userInput)
+    display.innerHTML = currentNum
+}
 
-
-
-
-//Calculator Display
-const display = document.getElementById("display")
 
 
